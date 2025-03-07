72192b6754f6af7597a67cb9bc783c7ff7480351
jensen-benard <jensen.benard@protonmail.com>
2025-03-07 13:46:45 +1300

    add function comment

--- software/control/src/control.cpp
+++ software/control/src/control.cpp
@@ -12,14 +12,28 @@ LQRControlMethod::LQRControlMethod(float thetaArmGain, float thetaArmDotGain, fl
     this->trackingGain = trackingGain;
 }
 
+/**
+ * Calculates the control output for the rotary inverted pendulum system using
+ * Linear Quadratic Regulator (LQR) method.
+ * 
+ * Each sensor reading is converted to radians, and the output back to degrees for unit consistency.
+ * However, mathematically this is redundant since the unit conversion cancels out.
+ *
+ * @param armAngle The current angle of the arm in degrees.
+ * @param armAngularVelocity The current angular velocity of the arm in degrees per second.
+ * @param pendulumAngle The current angle of the pendulum in degrees.
+ * @param pendulumAngularVelocity The current angular velocity of the pendulum in degrees per second.
+ * @param referenceAngle The desired reference angle for the system in degrees.
+ * @return The control output in degrees, which will be used to actuate the system.
+ */
 float LQRControlMethod::getOutput(float armAngle, float armAngularVelocity, float pendulumAngle, float pendulumAngularVelocity, float referenceAngle) {
-    float output = - thetaPendulumGain * pendulumAngle
-                   - thetaPendulumDotGain * pendulumAngularVelocity
-                   - thetaArmGain * armAngle
-                   - thetaArmDotGain * armAngularVelocity
-                   + trackingGain * referenceAngle;
     
-    return rad2deg(output) * PI / 7200;
+    float output = - thetaPendulumGain * deg2rad(pendulumAngle)
+                   - thetaPendulumDotGain * deg2rad(pendulumAngularVelocity)
+                   - thetaArmGain * deg2rad(armAngle)
+                   - thetaArmDotGain * deg2rad(armAngularVelocity)
+                   + trackingGain * deg2rad(referenceAngle);
+    return rad2deg(output) / 23;
 }
 
 LyapunovControlMethod::LyapunovControlMethod(float proportionalGain) {
